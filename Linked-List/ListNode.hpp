3ccb04eaeedb23bb581dda833d9e578916fb316c
Nathan Lam <nathancy@hawaii.edu>
2018-02-13 11:15:56 -1000

    Update linked list

--- Linked-List/ListNode.hpp
+++ Linked-List/ListNode.hpp
@@ -1,7 +1,6 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
-
 class ListNode {
     public:
         /* These two parameters should be in private scope
@@ -17,18 +16,10 @@ class ListNode {
         ListNode(int data);
         // Copy constructor
         ListNode(const ListNode& other);
-        // Move constructor
-        ListNode(ListNode&& other); 
-
+        
         // Getters
         int get_data() const;
-        ListNode* get_next() const;
-
-        // Setters
-        void set_next(ListNode* ptr);
-        void set_data(int num);
 };
 
 #endif 
-
 
--- Linked-List/linked-list-main.cpp
+++ Linked-List/linked-list-main.cpp
@@ -1,6 +1,6 @@
 /* Example for linked-list
  *
- * Compile with "g++ linked-list-main.cpp ListNode.cpp */
+ * Compile with "g++ linked-list-main.cpp ListNode.cpp" */
 
 #include <string>
 #include <iostream>
@@ -16,7 +16,7 @@ int main() {
 	// Length of the linked list
 	int length = 0;
 
-	// Creating a linked list
+	// Creating a linked list with three additional nodes
 	for(int i = 0; i < 3; i++) {
 		// Set current to a new node	
 		current->next = new ListNode(i);
@@ -28,8 +28,8 @@ int main() {
 
 	// Print out the linked list 	
     // Create a new pointer so you can iterate through list
-	for(ListNode *node = head; node != nullptr; node = node->next) {
-		std::cout << node->get_data() << " -> ";
+	for(ListNode *it = head; it != nullptr; it = it->next) {
+		std::cout << it->get_data() << " -> ";
 	} 
 	std::cout << "nullptr" << std::endl;
 }
